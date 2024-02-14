236d1680ac215335114b70da6433e704ff4101d8
vinnyegon90 <vinnyegon90@gmail.com>
2024-02-14 12:18:13 +0300

    Done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 
 
-const Navbar = () => {
+const Navbar = (setCategory) => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
@@ -11,21 +11,36 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <div className="nav-link" onClick={()=>setCategory("technology")}>Technology</div>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <div className="nav-link" onClick={()=>setCategory("business")}>Business</div>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <div className="nav-link" onClick={()=>setCategory("health")}>Health</div>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+          <div className="nav-link" onClick={()=>setCategory("sports")}>Sports</div>
         </li>
+        <li className="nav-item">
+          <div className="nav-link" onClick={()=>setCategory("entertaiment")}>Entertainment</div>
+        </li>
+        <li className="nav-item">
+          <div className="nav-link" onClick={()=>setCategory("sign up")}>Sign Up</div>
+        </li>
+        <li className="nav-item">
+          <div className="nav-link" onClick={()=>setCategory("register")}>Register</div>
+        </li>
+        <li className="nav-item">
+        <button className="btn btn-outline-light" onClick={(setCategory)} type="submit">Search</button>
+        </li>   
+             
       </ul>
+      
     </div>
   </div>
 </nav>
+
 
   )
 }
--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,16 +1,16 @@
-import { data } from "jquery";
-import { useEffect, useState } from "react"
+import { useEffect } from "react"; 
+import { useState } from "react" 
 import NewsItem from "./NewsItem";
 
-const NewsBoard = () => {
+const NewsBoard = ({category}) => {
 
-    const [articles,seArticles] = useState([]);
+    const [articles,setArticles] = useState([]);
 
     useEffect(()=>{
-        let url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
-        fetch(url).then(response=> response.json()).then(data=> seArticles(data.articles));
+        let url =`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
 
-    },[])
+    },[category])
 
   return (
     <div>
